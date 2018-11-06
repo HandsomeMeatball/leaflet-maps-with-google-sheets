3a028c1f9981d6589655eee9bcf9ac0156b05129
HandsomeMeatball <44821028+HandsomeMeatball@users.noreply.github.com>
2018-11-06 14:34:34 -0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GGYucDYtC3te7rmqFQOzOm9tq9lwrcMDl4dGV7CRuV4/edit#gid=0';
